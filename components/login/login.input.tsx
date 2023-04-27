9a6678d90b4dcb1d9f64e086ae460805d003abfc
Jeremy <jeremy.fran@icloud.com>
2023-04-27 16:36:03 +0800

    fix: login input

--- components/login/login.input.tsx
+++ components/login/login.input.tsx
@@ -1,19 +1,19 @@
-import React, {FC} from 'react';
-import {useFormContext} from "react-hook-form";
-import {LoginValidation} from "@/utils/formValidations";
-import {LoginInputs} from "@/pages/login";
+import React, { FC } from "react";
+import { useFormContext } from "react-hook-form";
+import { LoginValidation } from "@/utils/formValidations";
 
 const LoginInput: FC = () => {
-    const { register } = useFormContext<LoginInputs>();
+  const { register } = useFormContext();
 
-    return (
-        <div className={'flex flex-col'}>
-            <label htmlFor={'username'}>
-                Email/Username
-            </label>
-            <input {...register('username', LoginValidation.username)} className={''} />
-        </div>
-    );
+  return (
+    <div className={"flex flex-col"}>
+      <label htmlFor={"username"}>Email/Username</label>
+      <input
+        {...register("username", LoginValidation.username)}
+        className={""}
+      />
+    </div>
+  );
 };
 
 export default LoginInput;
